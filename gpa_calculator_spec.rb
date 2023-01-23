60b2e0837559d984a6a081df6526c01c1355e3cf
Emma Beynon <emma@coveragebook.com>
2023-01-23 08:55:40 +0000

    ADD TESTS

---
+++ gpa_calculator_spec.rb
@@ -0,0 +1,33 @@
+require "rspec"
+require_relative "gpa_calculator"
+
+RSpec.describe GpaCalculator do
+  describe "#gpa" do
+    subject { described_class.new(name, grades).gpa }
+    let(:name) { %w[Andy Dan Emma Olivia Ollie Mark].sample }
+
+    context "valid grades provided" do
+      let(:grades) { %w[A B+ C] }
+
+      it "returns GPA score" do
+        expect(subject).to eq(3.1)
+      end
+    end
+
+    context "invalid grades provided" do
+      let(:grades) { %w[X Y Z] }
+
+      it "returns 0.0" do
+        expect(subject).to eq(0.0)
+      end
+    end
+
+    context "valid and invalid grades provided" do
+      let(:grades) { %w[X B- D] }
+
+      it "returns GPA score" do
+        expect(subject).to eq(1.2)
+      end
+    end
+  end
+end
