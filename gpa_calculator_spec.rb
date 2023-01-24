eb5a80ff93f43969d2ddcf5c9fae06f24bd8a411
Emma Beynon <emma@coveragebook.com>
2023-01-24 11:25:33 +0000

    Add GpaCalculator

---
+++ gpa_calculator_spec.rb
@@ -0,0 +1,51 @@
+require "rspec"
+require_relative "gpa_calculator"
+
+RSpec.describe GpaCalculator do
+  describe "#gpa" do
+    subject { described_class.new(name, grades).gpa }
+    let(:name) { %w[Andy Dan Emma Olivia Ollie Mark].sample }
+
+    context "array of valid grades provided" do
+      let(:grades) { %w[A B+ C] }
+
+      it "returns GPA score" do
+        expect(subject).to eq(3.1)
+      end
+    end
+
+    context "array of invalid grades provided" do
+      let(:grades) { %w[X Y Z] }
+
+      it "returns 0.0" do
+        expect(subject).to eq(0.0)
+      end
+    end
+
+    context "array of valid and invalid grades provided" do
+      let(:grades) { %w[X B- D] }
+
+      it "returns GPA score" do
+        expect(subject).to eq(1.2)
+      end
+    end
+
+    context "string of valid grades provided" do
+      let(:grades) { "A B+ C" }
+
+      it "returns GPA score" do
+        expect(subject).to eq(3.1)
+      end
+    end
+  end
+
+  describe "#announcement" do
+    subject { described_class.new(name, grades).announcement }
+    let(:name) { %w[Andy Dan Emma Olivia Ollie Mark].sample }
+    let(:grades) { %w[A B+ C] }
+
+    it "returns string with name and GPA score" do
+      expect(subject).to eq("#{name} scored an average of 3.1")
+    end
+  end
+end
